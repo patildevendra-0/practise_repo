a44e402d4b37f5949744383d1e409328cbb43d8e
patildevendra-0 <patildev2001@gmail.com>
2024-01-17 11:27:20 +0530

    new

--- Program_10.c
+++ Program_10.c
@@ -1,62 +1,52 @@
 #include <stdio.h>
 #include <dirent.h>
-#include<string.h>
+#include <string.h>
 #include <sys/stat.h>
 
 int main(int argc, char *argv[])
 {
-    char File_Name[30];
+
+    char Path[30] = {'\0'};
     DIR *dp = NULL;
     struct dirent *entry = NULL;
     struct stat sobj;
 
-    if (argc != 2)
-    {
-        printf("INSUFFICIENT ARGUMENTS...\n");
-        return -1;
-    }
-
     dp = opendir(argv[1]);
     if (dp == NULL)
     {
-        printf("UNABLE TO OPEN DIRECTORY...\n");
+        printf("Unable to open directory...\n");
         return -1;
     }
 
     while ((entry = readdir(dp)) != NULL)
     {
-        sprintf(File_Name, "%s/%s", argv[1], entry->d_name);
-        stat(File_Name, &sobj);
+        sprintf(Path, "%s/%s", argv[1], entry->d_name);
+        stat(Path, &sobj);
 
-        if((strcmp(entry->d_name,".")==0 ||(strcmp(entry->d_name,"..")==0 )))
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
         {
             continue;
         }
 
         if (S_ISDIR(sobj.st_mode))
-        {   
-            
+        {
+            DIR * sub_dp = NULL;
 
-            DIR *sub_dp = opendir(File_Name);
-            if (sub_dp == NULL)
-            {
-                printf("UNABLE TO OPEN SUBDIRECTORY...\n");
-                return -1;
-            }
+            sub_dp = opendir(Path);
 
             while ((entry = readdir(sub_dp)) != NULL)
             {
-                printf("%s\n", entry->d_name);
+                printf("%s directory File:%s\n ",Path,entry->d_name);
             }
+
 
             closedir(sub_dp);
         }
         else
         {
-            printf("%s\n", entry->d_name);
+            printf("%s directory File:%s\n ",Path,entry->d_name);
         }
     }
-
     closedir(dp);
     return 0;
 }
